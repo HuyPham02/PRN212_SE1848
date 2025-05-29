69ce9dd978c6c2e29116b22618ba85753fb052ee
HuyPham02 <huyplnse183324@fpt.edu.vn>
2025-05-29 22:22:11 +0700

    Add files via upload

---
+++ NestedLoop_Sort/Program.cs
@@ -0,0 +1,40 @@
+﻿void swap(ref int  x , ref int y)
+{
+    int temp = x;
+    x = y;
+    y = temp;
+}
+int[] values = new int[5];
+void create_array(int[] values)
+{
+    Random rd = new Random();
+    for (int i = 0; i < values.Length; i++)
+    {
+        values[i] = rd.Next(100);
+    }
+}
+
+void sort_array(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        for (int j = 0; j < arr.Length; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                swap(ref arr[i], ref arr[j]);
+            }
+        }
+
+    }
+}
+void print_array(int[] values)
+{
+    foreach (int value in values)
+        Console.WriteLine($"{value}\t");
+}
+create_array(values);
+print_array(values);
+sort_array(values);
+Console.WriteLine("\nAFTER SORTING:");
+print_array(values);    
